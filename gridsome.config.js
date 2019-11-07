8fc53ad8cac48effcbd0e32c1727737dcc71c617
princy <princy.raza@gmail.com>
2019-11-07 22:42:51 +0100

    Change config for Netlify CMS

--- gridsome.config.js
+++ gridsome.config.js
@@ -27,12 +27,7 @@ module.exports = {
     {
       use: `gridsome-plugin-netlify-cms`,
       options: {
-        modulePath: `src/admin/index.js`,
-        configPath: `src/admin/config.yml`,
-        htmlPath: `src/admin/index.html`,
-        publicPath: `/admin`,
-        htmlTitle: `My CMS`,
-        enableIdentityWidget: true
+        publicPath: `/admin`
       }
     }
   ]
